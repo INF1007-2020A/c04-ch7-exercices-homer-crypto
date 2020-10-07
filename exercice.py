9621bfb3b89d3a86139bc1f6708c36452c5efb66
homer-crypto <homer.nde-tsakou@polymtl.ca>
2020-10-07 10:25:37 -0400

    meh

--- exercice.py
+++ exercice.py
@@ -2,13 +2,20 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
-
+from math import pi
 
 # TODO: Définissez vos fonction ici
+
+def vol_mass(a = 2, b = 4, c = 2, p = 10 ):
+    volume = (4/3) * pi * a * b * c
+    masse = p * volume
+
+    return volume, masse
+
+#TODO: À partir d’une phrase donnée par l’utilisateur, écrire un programme qui affiche toutes les lettres
+# qui sont utilisées plus de 5 fois dans la phrase, en ordre décroissant d’utilisation (le plus fréquent en premier).
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
-    pass
+    print(vol_mass())
